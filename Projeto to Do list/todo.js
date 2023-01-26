05779de02a23cc77d0b138ff886171b2e6ed873e
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2023-01-26 15:26:54 -0300

    ajustando para conferir de há dados salvos no local storage

--- Projeto to Do list/todo.js
+++ Projeto to Do list/todo.js
@@ -1,18 +1,22 @@
-function verificarLocalStorage (){
+let tarefas = [];
+
+function verificarLocalStorage() {
     if (localStorage.getItem('listaTarefa')) {
-        alert("existem tarefas salvas");
         let listTarefaArray = localStorage.getItem('listaTarefa');
+        let partes = listTarefaArray.split(",");
 
-
-        let div = document.createElement('div');
-        let pTarefa = document.createElement('p');
         let list = document.getElementById('lista');
-        list.appendChild(div);
-        pTarefa.innerHTML = `${listTarefaArray}`;
-        div.appendChild(pTarefa);
+
+        for (let i = 0; i < partes.length; i++) {
+            let li = document.createElement('li')
+            let txtx = document.createTextNode(partes[i]);
+            list.appendChild(li);
+            li.appendChild(txtx);
+        }
     }
 }
 verificarLocalStorage();
+
 function addTarefa() {
     let inTarefa = document.getElementById('inTarefa');
     let tarefa = inTarefa.value;
@@ -20,46 +24,50 @@ function addTarefa() {
     let respost = document.getElementById("resposta");
 
 
-        if (tarefa == '') {
-            resp.innerHTML = `O campo está vazio, digite algo..`;
-            respost.style.backgroundColor = '#d12929';
-            respost.style.width = '300px';
-            respost.style.margin = '0 auto';
-            respost.style.color = '#fff';
-            respost.style.padding = '15px';
-            respost.style.borderRadius = '8px';
-            inTarefa.focus();
-            return;
-        }
-        respost.style.backgroundColor = '#ffffff';
-        resp.innerHTML = '';
-    
-        let lista = document.getElementById('lista');
-        let li = document.createElement('li');
-        let txt = document.createTextNode(tarefa);
-    
-        let checkbox = document.createElement('input');
-        checkbox.classList.add('checkbox');
-        checkbox.type = 'checkbox';
-        checkbox.id = 'checkbox';
-    
-        li.appendChild(checkbox);
-        li.appendChild(txt);
-        lista.appendChild(li);
-    
-        //salva no localStorage
-        //criando a array de obj
-        let listaTarefa = JSON.parse(localStorage.getItem('listaTarefa') || '[]')
-    
-        //dando um push a cada tarefa adicionada
-        listaTarefa.push({ tarefa: tarefa });
-    
-        //salva no localStorage
-        localStorage.setItem("listaTarefa", JSON.stringify(listaTarefa));
-    
-        inTarefa.value = '';
+    if (tarefa == '') {
+        resp.innerHTML = `O campo está vazio, digite algo..`;
+        respost.style.backgroundColor = '#d12929';
+        respost.style.width = '300px';
+        respost.style.margin = '0 auto';
+        respost.style.color = '#fff';
+        respost.style.padding = '15px';
+        respost.style.borderRadius = '8px';
         inTarefa.focus();
+        return;
     }
+    respost.style.backgroundColor = '#ffffff';
+    resp.innerHTML = '';
+
+    let lista = document.getElementById('lista');
+    let li = document.createElement('li');
+    let txt = document.createTextNode(tarefa);
+
+    let checkbox = document.createElement('input');
+    checkbox.classList.add('checkbox');
+    checkbox.type = 'checkbox';
+    checkbox.id = 'checkbox';
+
+    li.appendChild(checkbox);
+    li.appendChild(txt);
+    lista.appendChild(li);
+
+
+
+    //salva no localStorage
+    //criando a array de obj
+    let listaTarefa = JSON.parse(localStorage.getItem('listaTarefa') || '[]')
+
+    //dando um push a cada tarefa adicionada
+    listaTarefa.push({ tarefa: tarefa });
+
+    //salva no localStorage
+    localStorage.setItem("listaTarefa", JSON.stringify(listaTarefa));
+
+    //alert(listaTarefa[0].tarefa);
+
+    inTarefa.value = '';
+    inTarefa.focus();
+}
 
 
 let btAdd = document.getElementById('btAdd');
@@ -83,7 +91,7 @@ function excluir() {
 let btExcluir = document.getElementById('btExcluir');
 btExcluir.addEventListener('click', excluir);
 
-
+/*
 function bgDark() {
     document.querySelector('.dark-theme-border').classList.toggle('animation');
 
@@ -105,7 +113,7 @@ function li() {
 }
 li();
 
-
+*/
 
 
 
